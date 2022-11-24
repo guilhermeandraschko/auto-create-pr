d8ee19c767fc2a44d2c37c82821172406b013735
Guilherme Andraschko <guilhermeandraschko@Guilhermes-MBP.lan>
2022-11-24 16:08:04 +0000

    handling exception

--- README.md
+++ README.md
@@ -1,10 +1,11 @@
 ### Instructions
 
+- Install npm
 - Create a file called '.config' 
 - Add this in it: 
-owner = v-squad
-repo = v-platform
-body = This pull request is a test!
-base = master
-token = [your personal access token]
+<pre><p>owner = v-squad</p>
+<p>repo = v-platform</p>
+<p>body = This pull request is a test!</p>
+<p>base = master</p>
+<p>token = [your personal access token]</p></pre>
 - Run npm passing your branch name `npm start branch-name`
--- main.js
+++ main.js
@@ -1,30 +1,38 @@
 import { Octokit } from "@octokit/core";
 import * as fs from "fs";
 
+const readConfigFile = () => {
+    const configFile = fs.readFileSync('.config', 'utf-8');
+    const lines = configFile.split('\n');
+    return lines.reduce((argsmap, line) => {
+            let argmap = line.split('=');
+            let key = argmap[0]?.trim();
+            let value = argmap[1]?.trim();
+            argsmap.set(key, value);
+            return argsmap;
+        }, new Map());
+}
+
 console.log('reading config file ...');
-const configFile = fs.readFileSync('.config', 'utf-8');
-const lines = configFile.split('\n');
-const argsvars = lines.reduce((argsmap, line) => {
-        let argmap = line.split('=');
-        let key = argmap[0].trim();
-        let value = argmap[1].trim();
-        argsmap.set(key, value);
-        return argsmap;
-    }, new Map());
+const configvars = readConfigFile();
 console.log('done.');
+
 const args = process.argv.slice(2);
-const card = args[0];
+const branch = args[0];
 
 console.log('creating pr...');
-const octokit = new Octokit({ auth: argsvars.get('token') }),
-        owner = argsvars.get('owner'),
-         repo = argsvars.get('repo'),
-        title = `[${card || 'PR'}]`,
-        body  = `${card} ${argsvars.get('body')}`,
-        head  = `${card}`,
-        base  = argsvars.get('base');
-
-const response = await octokit.request(
-    `POST /repos/{owner}/{repo}/pulls`, { owner, repo, title, body, head, base }
-);
-console.log('done. Go to: ' + response.data.html_url);
+const octokit = new Octokit({ auth: configvars.get('token') }),
+        owner = configvars.get('owner'),
+         repo = configvars.get('repo'),
+        title = `[${branch || 'PR'}]`,
+        body  = `${branch} ${configvars.get('body')}`,
+        head  = `${branch}`,
+        base  = configvars.get('base');
+try {
+    const response = await octokit.request(
+        `POST /repos/{owner}/{repo}/pulls`, { owner, repo, title, body, head, base }
+    );
+    console.log('done. Go to: ' + response.data.html_url);
+} catch(e) {
+    console.log('failed. Response ' + e.message);
+}
