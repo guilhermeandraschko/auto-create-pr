277e749ea99bbbe36dc7ddabb2222ac3c34efce9
Guilherme Andraschko <guilhermeandraschko@Guilhermes-MBP.lan>
2022-11-29 17:23:54 +0000

    allow draft pr

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-import { Octokit } from "@octokit/core";
+import { Octokit } from "@octokit/rest";
 import getBody from "./get-body.js";
 import getCommandLineArgs from "./get-command-line-args.js";
 import getJiraBodyVars from "./get-jira-body-vars.js";
@@ -11,6 +11,8 @@ console.log('done.');
 console.log('reading command line args ...');
 const args = getCommandLineArgs();
 const branch = args[0];
+const draftarg = args[1];
+const isdraft = draftarg == 'draft' ? true : false;
 console.log('done.');
 
 console.log('rendering body template ...');
@@ -19,19 +21,19 @@ const bodyy = await getBody(configvars, bodyvars);
 console.log('done.');
 
 console.log('creating pr...');
-const octokit = new Octokit({ auth: configvars.get('token') }),
-        owner = configvars.get('owner'),
-         repo = configvars.get('repo'),
-        title = `[${branch + " - " + bodyvars.description + "]" || 'PR'}`,
-        body  = `${bodyy}`,
-        head  = `${branch}`,
-        base  = configvars.get('base');
+
 try {
-    const response = await octokit.request(
-        `POST /repos/{owner}/{repo}/pulls`, { owner, repo, title, body, head, base }
-    );
-    console.log('done. Go to: ' + response.data.html_url);
+    const octokit = new Octokit({ auth: configvars.get('token') });
+    const { data: pullRequest } = await octokit.rest.pulls.create({
+        owner: configvars.get('owner'),
+        repo: configvars.get('repo'),
+        title: `[${branch + " - " + bodyvars.description + "]" || 'PR'}`,
+        body: `${bodyy}`,
+        head: `${branch}`,
+        base: configvars.get('base'),
+        draft: isdraft
+    });
+    console.log('done. Go to: ' + pullRequest.html_url);
 } catch(e) {
     console.log('failed. Response ' + e.message);
 }
-
